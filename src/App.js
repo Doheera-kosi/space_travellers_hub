ab4820abf42d4f4a8ef1ff4278125345bf656ef0
tashisrael <tashisrael04@gmail.com>
2022-11-23 12:11:22 +0300

    Fetch rockets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css';
 import './styles/header.css';
 import './styles/Rockets.css';
+import './styles/missionslist.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Profiles from './components/Profile';
+import Missions from './components/Missions';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={(<Rockets />)} />
+          <Route exact path="/Missions" element={(<Missions />)} />
           <Route exact path="/Profile" element={(<Profiles />)} />
         </Routes>
       </div>
